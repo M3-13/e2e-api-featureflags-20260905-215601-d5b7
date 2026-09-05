ecc6c638187756a5660dded434a9474c1f20c798
office-crew <development@office-crew.dev>
2026-09-05 22:11:02 +0200

    feat: Deterministische Rollout-Evaluierung implementieren
    Closes #4

--- evaluate.go
+++ evaluate.go
@@ -1,11 +1,43 @@
 package main
 
-import "net/http"
+import (
+	"hash/fnv"
+	"net/http"
+)
 
 func stableHash(key, user string) uint64 {
-	return 0
+	h := fnv.New64a()
+	_, _ = h.Write([]byte(key))
+	_, _ = h.Write([]byte{0})
+	_, _ = h.Write([]byte(user))
+	return h.Sum64()
 }
 
 func (a *API) handleEvaluate(w http.ResponseWriter, r *http.Request) {
-	writeError(w, http.StatusNotImplemented, "not implemented")
+	user := r.URL.Query().Get("user")
+	if user == "" {
+		writeError(w, http.StatusBadRequest, "user is required")
+		return
+	}
+
+	key := r.PathValue("key")
+	flag, ok := a.store.Get(key)
+	if !ok {
+		writeError(w, http.StatusNotFound, "flag not found")
+		return
+	}
+
+	enabled := false
+	switch {
+	case !flag.Enabled:
+		enabled = false
+	case flag.RolloutPercent >= 100:
+		enabled = true
+	case flag.RolloutPercent <= 0:
+		enabled = false
+	default:
+		enabled = stableHash(flag.Key, user)%100 < uint64(flag.RolloutPercent)
+	}
+
+	writeJSON(w, http.StatusOK, map[string]bool{"enabled": enabled})
 }
